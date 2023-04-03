a8ed318c6baae641f5ad15892a491e0c956bd080
Wilson Lin <code@wilsonl.in>
2023-04-03 12:04:03 +1000

    Pin versions

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 This is a fork of [Detectron2](https://github.com/facebookresearch/detectron2).
 
-- Supports PyTorch 2.0, Python 3.11, and CUDA 11.8.
+- Supports up to NVIDIA Ampere GPU arch, PyTorch 2.0, and Python 3.11.
 - Uses modern Poetry for dependency management and packaging.
 - Removes everything outside the core library.
 - Generates prebuilt wheels for Linux x86-64.
 
+CUDA 11.7 is used for PyTorch as it's the CUDA version marked as stable by the official [PyTorch Release Compatibility Matrix](https://pytorch.org/blog/deprecation-cuda-python-support/), and doesn't require using an extra Python index source or a different package to the official PyPI `torch`.
+
+## Getting started
+
+- You do not need to have CUDA toolkit installed, and any existing installation won't conflict with PyTorch, as it comes with its own copy.
+- Install `detectron2-neo` from PyPI.
+
 ## Differences to upstream
 
+- Packages are only available for one CUDA version. There are no CPU-only variants.
 - Tests and style checkers/formatters have been removed. This cleans up our fork and makes it easier to grok. We assume upstream code is correct and never change any core code in our fork.
 - Sample configs, datasets, demos, models, and projects have been removed. This simplifies the fork scope to only the core library.
 - Docs and READMEs have been pruned. They can still be found in the upstream unchanged.
--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ pushd "$(dirname "$0")" &>/dev/null
 repo="$PWD"
 rm -rf build dist
 
-# Any versions other than these defaults are **untested**.
-cuda="${CUDA_VERSION:-11.8}" # Should be `x.y`, not `x.y.z`.
+# WARNING: Any versions other than these defaults are **untested**.
+cuda="${CUDA_VERSION:-11.7}" # Should be `x.y`, not `x.y.z`.
 torch="${TORCH_VERSION:-2.0.0}" # Should be exact i.e. `x.y.z`.
 python="${PYTHON_VERSION:-3.11}" # Should be `x.y`, not `x.y.z`.
 
@@ -28,20 +28,24 @@ RUN apt -y install software-properties-common
 RUN add-apt-repository -y ppa:deadsnakes/ppa
 RUN apt -y install python$python-dev python$python-full
 RUN python$python -m ensurepip
-RUN python$python -m pip install torch==$torch --index-url https://download.pytorch.org/whl/cu$(echo "$cuda" | sed 's/\.//')
+RUN python$python -m pip install torch==$torch
 RUN apt -y install ninja-build
 RUN python$python -m pip install poetry2setup wheel
+# Derived from upstream dev/packaging/{pkg_helpers.bash,build_wheel.sh}.
+ENV FORCE_CUDA=1
+# We intentionally don't use PTX, as it's not recommended for performance reasons according to PyTorch: https://pytorch.org/docs/stable/cpp_extension.html#torch.utils.cpp_extension.CUDAExtension. We intend to update this package frequently, so we can release support for newer CC as necessary.
+ENV TORCH_CUDA_ARCH_LIST='3.7;5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6'
 EOD
 dockerTag="detectron2-builder-cuda$cuda-torch$torch"
 docker build --progress plain -t "$dockerTag" .
 # Use `-it` to ensure SIGINT and SIGTERM are passed.
+# Ignore any message like `No CUDA runtime is found, using CUDA_HOME=/usr/local/cuda`. It's emitted by the PyTorch extension builder and only means no GPU is running. See https://github.com/pytorch/pytorch/blob/35b33095397a11e86f94f1c6820745fd6af68ec5/torch/utils/cpp_extension.py#L116.
 docker run \
   --rm \
   -it \
   --mount "type=bind,src=$repo,dst=/repo" \
   --mount "type=tmpfs,dst=/.cache" \
   -w /repo \
-  -e "CUDA_HOME=/usr/loca/cuda-$cuda" \
   --user $EUID:$EUID \
   "$dockerTag" \
   bash -c "mkdir dist && \
